52e22d05de5cecbdb2379f463ffb188ab1f2ccf5
onetiger02 <onetiger02@gmail.com>
2023-10-12 19:34:03 +0900

    [ilho] : 3주차 출석과제

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,13 +1,55 @@
 //IterationSample.js
 
 import React from "react";
+import { useState } from "react";
 
 const IterationSample = () => {
-  const names = ["눈사람", "얼음", "눈", "바람"];
 
-  const nameList = names.map((name) => <li>{name}</li>);
+  const [names, setNames] = useState([
+    {id:1, text: 'ajou'},
+    {id:2, text: 'apple'},
+    {id:3, text: 'google'},
+    {id:4, text: 'samsung'},
+]);
 
-  return <ul>{nameList}</ul>;
+  const [inputText, setInputText] = useState("");
+
+  // 새로운 상태 id값을 관리하고자 하는 hook
+  const [nextId, setNextId] = useState(5);
+
+  const onCHangeText = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+
+
+  const onClick = () => {
+    console.log("onCLick event가 실행됨")
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId +1);
+    setNames(nextNames);
+    setInputText("");
+  };
+
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const nameList = names.map((name) => (
+  <li key={name.id} onDoubleClick={() => onRemove(name.id)}> {name.text}</li>));
+
+
+  return (
+    <>
+    <input value={inputText} onChange={onCHangeText}/>
+    <button onClick={onClick}> 추가</button>
+    <ul>{nameList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
